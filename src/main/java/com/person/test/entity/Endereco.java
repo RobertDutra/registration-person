18594c0adb9988999c01b135b0b33e6d5700f7b4
Robert Dutra <robertdutra10@hotmail.com>
2023-01-04 18:29:08 -0300

    Entity endereço criado

--- src/main/java/com/person/test/entity/Endereco.java
+++ src/main/java/com/person/test/entity/Endereco.java
@@ -1,29 +1,22 @@
 package com.person.test.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import java.util.Date;
-import java.util.List;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
 
-public class Person {
-
+public class Endereco {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String nome;
 
-    private Date dataNascimento;
-
-    @ManyToOne
-    @JoinColumn(name = 'endereco_id')
-    private List<Endereco> Enderecos;
 }
